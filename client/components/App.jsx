27ed3dfea7f845c7fa0e69fdd700d05a3e5a44c9
James Ferrell <jamesferrell@Jamess-MBP.lan1>
2020-12-19 12:36:16 -0500

    redux

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,7 +8,6 @@ const App = () => {
     return (
         <div>
             <ShoppingContainer />
-            <ShoppingItem />
         </div>
     )
 }
--- client/components/ShoppingContainer/ShoppingContainer.jsx
+++ client/components/ShoppingContainer/ShoppingContainer.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import ShoppingItem from './ShoppingItem.jsx';
 
 export const ShoppingContainer = () => {
     return (
         <div>
            <h1>Shopping List</h1> 
+           <ShoppingItem />
         </div>
     )
 }
