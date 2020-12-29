be460ac7180a8e1ec50b4316d83b66312c09348b
stevehong423 <stevehong423@hotmail.com>
2020-12-28 18:57:39 -0800

    first merge

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 // import './ShoppingContainer/ShoppingContainer.jsx';
 import { ShoppingContainer } from './ShoppingContainer/ShoppingContainer.jsx';
 import { ShoppingItem } from './ShoppingContainer/ShoppingItem.jsx';
-
+import { Header } from './Header/Header.jsx';
 
 const App = () => {
     return (
         <div>
+            <Header />
             <ShoppingContainer />
             <ShoppingItem />
         </div>
